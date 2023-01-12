80d223d85c3a986350da45964fc7d9de0fb46d6c
snpwflake <timyr.278@gmail.com>
2023-01-12 13:45:12 +0500

    Функция для подсчета строки без содержания ссылок

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-123
+const message1 = 'Всем привет!';
+postSize(message1); // вернет 12
+const message2 = 'Привет! https://github.com';
+postSize(message2); // вернет 8
+
+function postSize(message){
+    let result;
+    let str = message.indexOf('https://')
+    if (str < 0){
+        result = message.length;
+    }
+    else{
+        result = str
+    }
+    console.log(result);
+    return result;
+}
