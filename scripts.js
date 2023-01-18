d854b90632dab70ecb5b3558210b06264e99b26a
snpwflake <timyr.278@gmail.com>
2023-01-18 15:02:52 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,18 +6,18 @@ postSize(message2); // вернет 8
 
 function postSize(message){
   let result;
-  let str = message.split(' ');
-  let arr = [];
+  const str = message.split(' ');
+  const arr = [];
   let i = 0;
   str.forEach(element => {
-    if (element.indexOf('https://') < 0 ){
-      arr.push(element)
-    }
+    if (element.indexOf('https') < 0 && element.indexOf('http') < 0 && element.indexOf('www.') < 0){
+      arr.push(element);
+    } 
     else{
-      i += 1;
-    }
+      i+=1;
+    } 
   });
   result = arr.join(' ').length + i;
-  console.log(result)
+  console.log(result);
   return result;
 }
