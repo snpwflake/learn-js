c67e646c41394f5f3cc765566be05a7e0cb95b30
Timur Basyrov <timyr.278@gmail.com>
2023-03-17 14:39:41 +0500

    Фича для подсветки хэштегов в строках

---
+++ tests/hashtags.test.js
@@ -0,0 +1,23 @@
+import { assert } from 'chai';
+import HashTag from '../public/assets/hashtags.js';
+
+describe('Функция для нахождения хэштегов', function () {
+  it('Строка без хэштега', function () {
+    assert.equal('Кто еще изучает хэштег?', HashTag('Кто еще изучает хэштег?'));
+  });
+  it('Строка с хэштегом', function () {
+    assert.equal('Кто еще изучает <a href="/search?tag="javascript">#javascript</a> ?', HashTag('Кто еще изучает #javascript ?'));
+  });
+  it('Строка из хэштегов', function () {
+    assert.equal('<a href="/search?tag="победа">#победа</a> <a href="/search?tag="Tesla">#Tesla</a> <a href="/search?tag="hello">#hello</a> <a href="/search?tag="Molodec">#Molodec</a>', HashTag('#победа #Tesla #hello #Molodec'));
+  });
+  it('Строка с хэштегами', function () {
+    assert.equal('Кто еще изучает <a href="/search?tag="победа">#победа</a> <a href="/search?tag="Tesla">#Tesla</a>', HashTag('Кто еще изучает #победа #Tesla'));
+  });
+  it('Пустая строка', function () {
+    assert.equal('', HashTag(''));
+  });
+  it('Строка из одного хэштега', function () {
+    assert.equal('<a href="/search?tag="LoL">#LoL</a>', HashTag('#LoL'));
+  });
+});
