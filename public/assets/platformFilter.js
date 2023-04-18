212ad95f199850e2805fbda51770df93316e6b46
Timur Basyrov <timyr.278@gmail.com>
2023-04-18 13:24:05 +0500

    fix

--- public/assets/platformFilter.js
+++ public/assets/platformFilter.js
@@ -3,16 +3,15 @@ import censorship from './badWords.js';
 export default function platformFilter(InnerText) {
   const CensoreWords = censorship();
   const Text = InnerText.split(' ');
-  CensoreWords.forEach((BadWord) => {
-    for (let i = 0; i < Text.length; i += 1) {
-      const box = Text[i].toLowerCase();
-      if (box.includes(BadWord) === true) {
-        const boxIndex = [box.indexOf(BadWord), BadWord.length];
-        const censore = Text[i].substr(boxIndex[0], boxIndex[1]);
-        Text[i] = Text[i].substr(0, boxIndex[0]) + '*'.repeat(censore.length) + Text[i].substr(boxIndex[1]);
+  for (let i = 0; i < Text.length; i += 1) {
+    const word = Text[i].toLowerCase();
+    CensoreWords.forEach((badWord) => {
+      if (word.includes(badWord) === true) {
+        const IndexBadWord = [word.indexOf(badWord), badWord.length];
+        Text[i] = Text[i].substr(0, IndexBadWord[0]) + '*'.repeat(IndexBadWord[1]) + Text[i].substr(IndexBadWord[1] + 1);
       }
-    }
-  });
+    });
+  }
   console.log(Text.join(' '));
   return Text.join(' ');
 }
