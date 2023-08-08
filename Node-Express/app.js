85a6c362d0709c4e4c83e2826b55dd934d377742
Timur Basyrov <timyr.278@gmail.com>
2023-08-08 23:29:04 +0500

    Update app.js

--- Node-Express/app.js
+++ Node-Express/app.js
@@ -1,13 +1,7 @@
-const express = require("express");
+const express = require("./express");
 const app = express();
+
 const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.type('html').send(html));
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
 
 const html = `
 <!DOCTYPE html>
@@ -58,4 +52,11 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
+
+app.get('/', (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
