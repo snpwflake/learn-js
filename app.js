10a86e3a6f682918589842d444a6c8bcff268400
Timur Basyrov <timyr.278@gmail.com>
2023-08-10 21:53:25 +0500

    test
    test

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+import pg from 'pg';
+
+const client = new pg.Client({
+  user: 'twitbtn_production_user',
+  host: 'dpg-cjaaiei683bs73asugs0-a',
+  database: 'twitbtn_production',
+  password: 'MtjFkxNmIfzPdQTSeojzKGanhNfQhXvB',
+  port: 5432,
+});
+
+client.connect();
