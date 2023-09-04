0d1186cf4f8d70e823cab355a1f1f2ad7a71abdc
Timur Basyrov <timyr.278@gmail.com>
2023-09-04 16:08:02 +0500

    Подключил PSQL к сайту, переделал функцию под PSQL, вывел данные

---
+++ app.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+import fs from 'fs';
+import pkg from 'pg';
+
+const { Client } = pkg;
+
+const client = new Client({
+  user: 'twitbtn_production_user',
+  host: 'dpg-cjaaiei683bs73asugs0-a.oregon-postgres.render.com',
+  database: 'twitbtn_production',
+  password: 'MtjFkxNmIfzPdQTSeojzKGanhNfQhXvB',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+await client.connect();
+
+const app = express();
+const port = 3000;
+
+const html = fs.readFileSync('public/main.html', 'utf8');
+
+app.get('/', (req, res) => res.type('html').send(html));
+
+app.get('/posts.json', async (req, res) => {
+  const db = await client.query('SELECT * FROM post');
+  res.send(`${JSON.stringify(db.rows)}`);
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
